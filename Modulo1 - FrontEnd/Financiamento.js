e3809b344841bfa67d5edb88f2275548a2226fc5
AHGW3 <yhugorocha@gmail.com>
2021-09-22 16:16:08 -0300

    iniciando javascript

---
+++ Modulo1 - FrontEnd/Financiamento.js
@@ -0,0 +1,9 @@
+//Variaveis para simulação
+var valor = document.getElementById('iValor');
+var prazo = document.getElementById('iPrazoAnos');
+var jurosAno = document.getElementById('dJurosAnos');
+
+
+console.log(valor.value);
+console.log(prazo.value);
+console.log(jurosAno.value);
