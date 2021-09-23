78d0acc779467582832aad13ce7d9d265159656a
AHGW3 <yhugorocha@gmail.com>
2021-09-23 16:54:58 -0300

    função calcular e estilização pag

--- Modulo1 - FrontEnd/Financiamento.js
+++ Modulo1 - FrontEnd/Financiamento.js
@@ -1,9 +1,22 @@
 //Variaveis para simulação
 var valor = document.getElementById('iValor');
-var prazo = document.getElementById('iPrazoAnos');
+var prazoAno = document.getElementById('iPrazoAnos');
 var jurosAno = document.getElementById('dJurosAnos');
 
 
-console.log(valor.value);
-console.log(prazo.value);
-console.log(jurosAno.value);
+function Calcular(){
+    //Calculo de juros anual para mensal
+    var dJurosMeses = Math.pow(1 + parseFloat(jurosAno.value),1/12) - 1;
+    document.getElementById("dJurosMeses").value = dJurosMeses;
+    //Calculo anos para meses
+    var iPrazosMeses = prazoAno.value * 12;
+    document.getElementById("iPrazosMeses").value = iPrazosMeses;
+    //Calculo juros acumulados
+    var dJurosAcumulados = dJurosMeses * iPrazosMeses;
+    document.getElementById("dJurosAcumulados").value = dJurosAcumulados;    
+}
+
+
+
+
+
