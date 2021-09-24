4367df5720988d80a03b97a29b10fb5a642807a6
AHGW3 <yhugorocha@gmail.com>
2021-09-23 21:21:49 -0300

    implementação da tabela com js

--- Modulo1 - FrontEnd/Financiamento.js
+++ Modulo1 - FrontEnd/Financiamento.js
@@ -2,9 +2,10 @@
 var valor = document.getElementById('iValor');
 var prazoAno = document.getElementById('iPrazoAnos');
 var jurosAno = document.getElementById('dJurosAnos');
+var corpoTabela = document.querySelector("tbody");    
 
 
-function Calcular(){
+    function Calcular(){
     //Calculo de juros anual para mensal
     var dJurosMeses = Math.pow(1 + parseFloat(jurosAno.value),1/12) - 1;
     document.getElementById("dJurosMeses").value = dJurosMeses;
@@ -13,8 +14,51 @@ function Calcular(){
     document.getElementById("iPrazosMeses").value = iPrazosMeses;
     //Calculo juros acumulados
     var dJurosAcumulados = dJurosMeses * iPrazosMeses;
-    document.getElementById("dJurosAcumulados").value = dJurosAcumulados;    
+    document.getElementById("dJurosAcumulados").value = dJurosAcumulados;
+    
+    var ValorPrestacao = valor.value / iPrazosMeses;
+    var saldoDevedor = valor.value;
+    var juros = [];
+
+    for(var i = 0; i < 5; i++){
+        var numprestacao = i + 1;
+        juros[i] = saldoDevedor * dJurosMeses;
+        var total = 0;
+
+        var linha = document.createElement("tr");
+        var campo_numPrestacao = document.createElement("td");
+        var campo_Amortizacao = document.createElement("td");
+        var campo_Juros = document.createElement("td");
+        var campo_Total = document.createElement("td");
+
+        var texto_numPrestacao = document.createTextNode(numprestacao);
+        var texto_Amortizacao = document.createTextNode(ValorPrestacao);
+        var texto_Juros = document.createTextNode(juros[i]);
+        var texto_Total = document.createTextNode(total);
+
+        campo_numPrestacao.appendChild(texto_numPrestacao);
+        campo_Amortizacao.appendChild(texto_Amortizacao);
+        campo_Juros.appendChild(texto_Juros);
+        campo_Total.appendChild(texto_Total);
+
+        linha.appendChild(campo_numPrestacao);
+        linha.appendChild(campo_Amortizacao);
+        linha.appendChild(campo_Juros);
+        linha.appendChild(campo_Total);
+
+        corpoTabela.appendChild(linha);
+
+        saldoDevedor = saldoDevedor - ValorPrestacao;
+    }
+
 }
+
+
+
+
+    
+
+
 
 
 
